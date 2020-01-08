ab51b3c828a0fa2b408b59626d9eda7ac2dd6e2e
kei-hamano <k-hamano@biglobe.co.jp>
2020-01-08 20:45:53 +0900

    2020/1/8分

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,2 +1,18 @@
 'use script'
-console.log('Hello World! from main')
+console.log('Hello World! from main');
+
+// 定数（const）
+const price = 150;
+console.log(price * 100);
+
+// 変数（let） ※古い書き方でvarもあるが使わない方がいい
+let kakaku = 180;
+console.log(kakaku * 110);
+kakaku = 200;
+console.log(kakaku * 120);
+
+// 型の参照
+console.log(typeof 'hello'); //string
+console.log(typeof 100); //number
+console.log(typeof true); //boolean
+
