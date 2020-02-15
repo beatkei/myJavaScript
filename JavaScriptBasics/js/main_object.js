ecc5a8d8cfbe5bc383a61c43e629fa42a42ff59c
beatkei <beatkei@gmail.com>
2020-02-16 07:24:50 +0900

    array追加

--- JavaScriptBasics/js/main_object.js
+++ JavaScriptBasics/js/main_object.js
@@ -13,4 +13,14 @@
     const otherScores1 = [30,100];
     const otherScores2 = [30,100, ...otherScores1];
     console.log(otherScores2);
+
+    // forEach 第一引数はカレントの値で、第二引数はindex、第三引数は配列自身 に自動的になり自由な名前を付けられる
+    const scores2 = [80, 90, 40, 70];
+    scores2.forEach((score, index) => {
+        console.log(`Score: ${index}:${score}`)
+    })
+
+    // map
+    const updateScores = scores2.map(score => score + 20 );
+    console.log(updateScores);
 }
